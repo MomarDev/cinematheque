0a213bfde68121e5a2f79f7beee2e837b194fff6
MomarDev <41522506+MomarDev@users.noreply.github.com>
2019-04-16 11:50:40 +0000

    Update ActeurControl.java
    Modification du controlleur Acteur

--- src/control/ActeurControl.java
+++ src/control/ActeurControl.java
@@ -7,44 +7,79 @@ package control;
 
 import java.util.*;
 
+import model.ActeurModel;
 import shared.Acteur;
+import view.*;
 
 /** @pdOid ada98af5-e68e-4220-a468-f30d3df53ab3 */
 public class ActeurControl {
    /** @param acteur
     * @pdOid bddb169f-4449-4084-95cd-0ca29fd31772 */
+	
+	
    public static void afficher(Acteur acteur) {
-      // TODO: implement
+
+
+	AfficherActeur fen = new AfficherActeur(acteur);
+	      JMain.afficher(fen);
+	   
    }
    
    /** @param acteur
     * @pdOid a0ca1096-3752-4fd0-8ce3-b289dcd98569 */
    public static void modifier(Acteur acteur) {
-      // TODO: implement
+
+	   ModifierActeur fen = new ModifierActeur(acteur);
+	      JMain.afficher(fen);
+
    }
    
    /** @pdOid 28fb4525-4e37-401c-ad96-1016ef2e16ae */
    public static void lister() {
-      // TODO: implement
+	   
+	   ArrayList<Acteur> acteurs = (ArrayList<Acteur>) ActeurModel.getActeurs();
+	   ListeActeurs fen = new ListeActeurs(acteurs);
+	      JMain.afficher(fen,true);
+	   
    }
    
    /** @param acteur
     * @pdOid 59357c88-4079-4316-9c7e-09cf6e2d88ff */
    public static void ajouterNouveau(Acteur acteur) {
       // TODO: implement
+	   if(!ActeurModel.addActeur(acteur)) {
+		   JMain.affcherErreur("Erreur lors de l'ajout! ");
+	   }
+	   else {
+		   lister();
+	   }
+	  
    }
    
    /** @param acteur
     * @pdOid f40763a7-f595-41f4-a1a8-4b13ba20bccf */
-   public static int enregistrerModification(Acteur acteur) {
-      // TODO: implement
-      return 0;
+   public static void enregistrerModification(Acteur acteur) {
+		   if(!ActeurModel.modifyActeur(acteur)) {
+			   JMain.affcherErreur("Erreur lors de la modification ");
+		   }
+		   else {
+			   lister();
+		   }
    }
    
    /** @param acteur
     * @pdOid ac4cbceb-cd09-40cf-b064-8247a4ad702b */
    public static void supprimer(Acteur acteur) {
-      // TODO: implement
+
+
+	   if(!ActeurModel.deleteActeur(acteur)) {
+		   JMain.affcherErreur("Erreur lors de la suppression ");
+	   }
+	   else {
+		   JMain.affcherInformation("Suppression reussie ");
+	   }
    }
+   
+   
 
 }
